e7e8545c0e5d99a171bd2b348d3faf4fa8cab08e
polidex <lp.igor@gmail.com>
2023-01-24 18:37:16 -0300

    feat: page Products

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,10 @@ import { Route, useHistory } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import Register from './pages/Register';
+<<<<<<< Updated upstream
+=======
+import Products from './pages/Products';
+>>>>>>> Stashed changes
 
 function App() {
   const history = useHistory();
@@ -12,6 +16,7 @@ function App() {
       <Route exact path="/" render={ () => history.push('/login') } />
       <Route exact path="/login" component={ Login } />
       <Route exact path="/register" component={ Register } />
+      <Route exact path="/products" component={ Products } />
     </>
   );
 }
---
+++ front-end/src/pages/Products.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react';
+// import { useHistory } from 'react-router-dom';
+// import { loginUser } from '../Api/api';
+import '../style/Products.css';
+
+function Products() {
+  return (
+    <main>
+      <h1>/Products</h1>
+    </main>
+  );
+}
+
+export default Products;
