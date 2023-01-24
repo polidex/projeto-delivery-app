f450eb0f2aa18665f66768fbe4a45598474de8b4
Laura Lana <lauramarqueslana@gmail.com>
2023-01-24 14:23:34 -0300

    fix: lint

--- back-end/src/utils/generateToken.js
+++ back-end/src/utils/generateToken.js
@@ -1,7 +1,8 @@
 require('dotenv/config');
 
 const jwt = require('jsonwebtoken');
-const secret = process.env.JWT_SECRET
+
+const secret = process.env.JWT_SECRET;
 const jwtConfig = {
   expiresIn: '7d',
   algorithm: 'HS256',
@@ -10,6 +11,6 @@ const jwtConfig = {
 const generateToken = (data) => {
   const token = jwt.sign({ data }, secret, jwtConfig);
   return token;
-}
+};
 
 module.exports = { generateToken };
