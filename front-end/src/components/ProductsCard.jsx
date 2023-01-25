8e68949354312733365bc0dd5aee8d0522dc6429
polidex <lp.igor@gmail.com>
2023-01-25 14:54:11 -0300

    feat: ajustes css products

--- front-end/src/components/ProductsCard.jsx
+++ front-end/src/components/ProductsCard.jsx
@@ -19,33 +19,35 @@ function ProductsCard() {
         data-testid="customer_products__img-card-bg-image-<id>"
         src="https://swiftbr.vteximg.com.br/arquivos/ids/184370-1500-1000/620031_HEINEKEN-269ML.png?v=637957499365500000"
       />
-      <div>
+      <div id="card-menu">
         <p
           id="card-title"
           data-testid="customer_products__element-card-title-<id>"
         >
           CERVEJA HEINEKEN 269ML
         </p>
-        <button
-          id="btn-rm"
-          type="button"
-          data-testid="customer_products__button-card-rm-item-<id>"
-        >
-          -
-        </button>
-        <input
-          id="int-qtt"
-          type="number"
-          data-testid="customer_products__input-card-quantity-<id>"
-          value="0"
-        />
-        <button
-          id="btn-add"
-          type="button"
-          data-testid="customer_products__button-card-add-item-<id>"
-        >
-          +
-        </button>
+        <div id="btns-menu">
+          <button
+            id="btn-rm"
+            type="button"
+            data-testid="customer_products__button-card-rm-item-<id>"
+          >
+            -
+          </button>
+          <input
+            id="int-qtt"
+            type="number"
+            data-testid="customer_products__input-card-quantity-<id>"
+            value="0"
+          />
+          <button
+            id="btn-add"
+            type="button"
+            data-testid="customer_products__button-card-add-item-<id>"
+          >
+            +
+          </button>
+        </div>
       </div>
     </div>
 
