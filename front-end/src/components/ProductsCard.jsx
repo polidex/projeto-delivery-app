e22bf8cac2b26e99dbeb83c156b2dc32aeb85f5b
Gabriel Tenari <gabriel.tenari.ismart@gmail.com>
2023-01-25 14:56:47 -0300

    feat: implementing productCard component

--- front-end/src/components/ProductsCard.jsx
+++ front-end/src/components/ProductsCard.jsx
@@ -1,47 +1,53 @@
 import '../style/components/ProductsCard.css';
+import { useState } from 'react';
 
-function ProductsCard() {
-
+function ProductsCard(id, price, name, url) {
+  const [products, setProducts] = useState({});
   const productsHandle = (target) => {
-    
-  }
+    console.log(target);
+    setProducts((prevState) => ({
+
+    }));
+  };
   return (
     <div id="card">
       <p
         id="price"
-        data-testid="customer_products__element-card-price-<id>"
+        data-testid={ `customer_products__element-card-price-${id}` }
       >
         R$4,49
       </p>
       <img
         id="image"
         alt="imagem do produto <id>"
-        data-testid="customer_products__img-card-bg-image-<id>"
+        data-testid={ `customer_products__img-card-bg-image-${id}` }
         src="https://swiftbr.vteximg.com.br/arquivos/ids/184370-1500-1000/620031_HEINEKEN-269ML.png?v=637957499365500000"
       />
       <div>
         <p
           id="card-title"
-          data-testid="customer_products__element-card-title-<id>">
+          data-testid={ `customer_products__element-card-title-${id}` }
+        >
           CERVEJA HEINEKEN 269ML
         </p>
         <button
           id="btn-rm"
           type="button"
-          data-testid="customer_products__button-card-rm-item-<id>"
+          data-testid={ `customer_products__button-card-rm-item-${id}` }
         >
           -
         </button>
         <input
           id="int-qtt"
           type="number"
-          data-testid="customer_products__input-card-quantity-<id>"
+          data-testid={ `customer_products__input-card-quantity-${id}` }
           value="0"
         />
         <button
+          onClick={ (event) => productsHandle(event.target) }
           id="btn-add"
           type="button"
-          data-testid="customer_products__button-card-add-item-<id>"
+          data-testid={ `customer_products__button-card-add-item-${id}` }
         >
           +
         </button>
--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -2,11 +2,13 @@
 // import { useHistory } from 'react-router-dom';
 // import { loginUser } from '../Api/api';
 import '../style/Products.css';
+import ProductsCard from '../components/ProductsCard';
 
 function Products() {
   return (
     <main>
       <h1>/Products</h1>
+      <ProductsCard />
     </main>
   );
 }
