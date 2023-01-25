bc18071f3c53923ae69f0036df8cdb38f8d53a21
polidex <lp.igor@gmail.com>
2023-01-25 14:34:47 -0300

    feat: css

--- front-end/src/components/ProductsCard.jsx
+++ front-end/src/components/ProductsCard.jsx
@@ -1,30 +1,45 @@
 import '../style/components/ProductsCard.css';
 
 function ProductsCard() {
+
+  const productsHandle = (target) => {
+    
+  }
   return (
-    <div>
-      <p data-testid="customer_products__element-card-price-<id>">R$4,49</p>
+    <div id="card">
+      <p
+        id="price"
+        data-testid="customer_products__element-card-price-<id>"
+      >
+        R$4,49
+      </p>
       <img
+        id="image"
         alt="imagem do produto <id>"
         data-testid="customer_products__img-card-bg-image-<id>"
         src="https://swiftbr.vteximg.com.br/arquivos/ids/184370-1500-1000/620031_HEINEKEN-269ML.png?v=637957499365500000"
       />
       <div>
-        <p data-testid="customer_products__element-card-title-<id>">
+        <p
+          id="card-title"
+          data-testid="customer_products__element-card-title-<id>">
           CERVEJA HEINEKEN 269ML
         </p>
         <button
+          id="btn-rm"
           type="button"
           data-testid="customer_products__button-card-rm-item-<id>"
         >
           -
         </button>
         <input
+          id="int-qtt"
           type="number"
           data-testid="customer_products__input-card-quantity-<id>"
           value="0"
         />
         <button
+          id="btn-add"
           type="button"
           data-testid="customer_products__button-card-add-item-<id>"
         >
