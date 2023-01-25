b454e773ea5ec145ea441dcd2d4023b89d66ac24
Kauan <kauan.s.leite@hotmail.com>
2023-01-25 14:44:29 -0300

    feat: Menu

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,12 +1,13 @@
 // import { useState } from 'react';
 // import { useHistory } from 'react-router-dom';
 // import { loginUser } from '../Api/api';
+import Menu from '../components/Menu';
 import '../style/Products.css';
 
 function Products() {
   return (
     <main>
-      <h1>/Products</h1>
+      <Menu />
     </main>
   );
 }
