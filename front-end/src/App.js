ced63f6c6193568435e5bfa633412a00e86d4ded
Kauan <kauan.s.leite@hotmail.com>
2023-01-31 15:44:11 -0300

    feat: details order seller

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,7 @@ import Register from './pages/Register';
 import Products from './pages/Products';
 import Checkout from './pages/Checkout';
 import Details from './pages/Details';
+import DetailsSeller from './pages/seller/Details';
 import Orders from './pages/Orders';
 
 function App() {
@@ -13,13 +14,19 @@ function App() {
 
   return (
     <>
+      { /* All */ }
       <Route exact path="/" render={ () => history.push('/login') } />
       <Route exact path="/login" component={ Login } />
       <Route exact path="/register" component={ Register } />
+
+      { /* Customer */ }
       <Route exact path="/customer/products" component={ Products } />
       <Route exact path="/customer/checkout" component={ Checkout } />
       <Route exact path="/customer/orders/:id" component={ Details } />
       <Route exact path="/customer/orders" component={ Orders } />
+
+      { /* Seller */ }
+      <Route exact path="/seller/orders/:id" component={ DetailsSeller } />
     </>
   );
 }
---
+++ front-end/src/pages/seller/Details.jsx
@@ -0,0 +1,105 @@
+import moment from 'moment/moment';
+import 'moment/locale/pt-br';
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import { getOrderByID } from '../../Api/api';
+import DetailsOrder from '../../components/seller/DetailsOrder';
+import Menu from '../../components/Menu';
+import '../../style/Details.css';
+
+function Details(props) {
+  const [infos, setInfos] = useState();
+  const { match } = props;
+  const { params } = match;
+  const { id } = params;
+
+  useEffect(() => {
+    async function fetchOrderByID() {
+      setInfos(await getOrderByID(id));
+    }
+    fetchOrderByID();
+  }, [id]);
+
+  const showOrderId = (orderId) => {
+    const TRES = 3;
+    switch (orderId.toString().length) {
+    case 1:
+      return `000${orderId}`;
+    case 2:
+      return `00${orderId}`;
+    case TRES:
+      return `0${orderId}`;
+    default:
+      return orderId;
+    }
+  };
+
+  const prepareOrder = (orderId) => {
+    console.log(`prepare order ${orderId}`);
+  };
+
+  const dispatchOrder = (orderId) => {
+    console.log(`pedido ${orderId} saiu pra entrega`);
+  };
+
+  const infosProduct = () => (
+    <div className="card-pedido">
+      <h1>Detalhe do Pedido</h1>
+      <div className="header-pedido">
+        <h4
+          data-testid="seller_order_details__element-order-details-label-order-id"
+        >
+          { `Pedido ${showOrderId(infos.data.id)}` }
+        </h4>
+        <h4
+          data-testid="seller_order_details__element-order-details-label-order-date"
+        >
+          {
+            moment(infos.data.saleDate).format('L')
+          }
+        </h4>
+        <h4
+          data-testid="seller_order_details__element-order-details-label-delivery-status"
+          className="status"
+        >
+          {infos.data.status}
+        </h4>
+        <button
+          type="button"
+          className="btn-status"
+          data-testid="seller_order_details__button-preparing-check"
+          onClick={ () => prepareOrder(infos.data.id) }
+        >
+          PREPARAR PEDIDO
+        </button>
+        <button
+          type="button"
+          className="btn-status"
+          data-testid="seller_order_details__button-dispatch-check"
+          onClick={ () => dispatchOrder(infos.data.id) }
+        >
+          SAIU PARA ENTREGA
+        </button>
+      </div>
+      <DetailsOrder
+        products={ infos.data.products }
+        totalPrice={ infos.data.totalPrice }
+      />
+    </div>
+  );
+
+  return (
+    <div>
+      <Menu />
+      { infos && infosProduct()}
+    </div>
+  );
+}
+
+Details.propTypes = {
+  match: PropTypes.shape.isRequired,
+  params: PropTypes.shape.isRequired,
+  id: PropTypes.number.isRequired,
+};
+
+export default Details;
